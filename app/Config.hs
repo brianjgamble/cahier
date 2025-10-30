9a415a518fea1a6222c8d450b9b30db2e9695398
Brian Gamble <brianjgamble@users.noreply.github.com>
2025-10-29 23:45:33 -0500

    Read port and host from the environment

---
+++ app/Config.hs
@@ -0,0 +1,27 @@
+module Config (webOptions) where
+
+import Data.Maybe (fromMaybe)
+import Data.String (fromString)
+import Data.String.Interpolate (i)
+import Network.Wai.Handler.Warp (HostPreference, defaultSettings, setBeforeMainLoop, setHost, setPort)
+import System.Environment (lookupEnv)
+import Web.Scotty
+
+webOptions :: IO Options
+webOptions = do
+  -- Read environment variables
+  maybePort <- lookupEnv "PORT"
+  maybeHost <- lookupEnv "HOST"
+
+  -- Convert and provide defaults
+  let port = maybe 3000 read maybePort
+      host = fromString (fromMaybe "127.0.0.1" maybeHost) :: HostPreference
+
+  -- Build Warp settings
+  let settings = setPort port $ setHost host $ defaultSettings
+
+  -- Optional: Log when the server starts
+  let settings_with_log = setBeforeMainLoop (putStrLn $ [i|Server running on #{host}:#{port}|]) settings
+
+  -- Scotty options
+  pure $ Options 0 settings_with_log -- first arg is verbose mode (0 = silent)
--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,38 @@
+import Config (webOptions)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Network.Wai.Middleware.Static
-import qualified Text.Blaze.Html.Renderer.Text as R
-import Web.Scotty
-
 import Templates.About (pageAbout)
 import Templates.Articles (pageArticles)
 import Templates.Blog (pageBlog)
 import Templates.Contact (pageContact)
 import Templates.Home (pageHome)
 import Templates.Layout (baseTemplate)
+import qualified Text.Blaze.Html.Renderer.Text as R
+import Web.Scotty
 
 main :: IO ()
-main = scotty 3000 do
-  middleware $ staticPolicy (noDots >-> addBase "static")
+main = do
+  opts <- webOptions
+  scottyOpts opts do
+    middleware $ staticPolicy (noDots >-> addBase "static")
+    middleware logStdoutDev
 
-  get "/" $
-    html . R.renderHtml $
-      baseTemplate "Home" pageHome
+    get "/" $
+      html . R.renderHtml $
+        baseTemplate "Home" pageHome
 
-  get "/blog" $
-    html . R.renderHtml $
-      baseTemplate "Blog" pageBlog
+    get "/blog" $
+      html . R.renderHtml $
+        baseTemplate "Blog" pageBlog
 
-  get "/articles" $
-    html . R.renderHtml $
-      baseTemplate "Articles" pageArticles
+    get "/articles" $
+      html . R.renderHtml $
+        baseTemplate "Articles" pageArticles
 
-  get "/contact" $
-    html . R.renderHtml $
-      baseTemplate "Contact" pageContact
+    get "/contact" $
+      html . R.renderHtml $
+        baseTemplate "Contact" pageContact
 
-  get "/about" $
-    html . R.renderHtml $
-      baseTemplate "About" pageAbout
+    get "/about" $
+      html . R.renderHtml $
+        baseTemplate "About" pageAbout
