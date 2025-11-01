968035d6891b4222f81c32cc35626e794438d71b
Brian Gamble <brianjgamble@users.noreply.github.com>
2025-11-01 00:20:22 -0500

    Let's get picocss from CDN

--- src/Templates/Layout.hs
+++ src/Templates/Layout.hs
@@ -14,7 +14,7 @@ layoutTemplate pageTitle content =
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="color-scheme" content="light dark">
     <title>#{pageTitle}
-    <link rel="stylesheet" href="css/pico.sand.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.sand.min.css">
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/theme-switcher.js">
     <script type="module" src="/js/app.js">
