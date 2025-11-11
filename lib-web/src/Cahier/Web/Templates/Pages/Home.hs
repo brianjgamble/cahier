737f925bdae24405b4d1804dcd0a0ee9c236e860
Brian Gamble <brianjgamble@users.noreply.github.com>
2025-11-10 23:21:33 -0600

    Add social links

--- lib-web/src/Cahier/Web/Templates/Pages/Home.hs
+++ lib-web/src/Cahier/Web/Templates/Pages/Home.hs
@@ -15,4 +15,12 @@ pageHome =
       Believer &bullet; Developer &bullet; Writer
   <p>
     Building software is my bread and butter, but I'm also a writer.  I write poetry, and I'm working on my own fantasy world and stories.
+
+  <article .social>
+    <header>
+      Social Links
+    <ul>
+      <li><a href="https://github.com/brianjgamble" target="_blank" rel="noreferrer noopener">GitHub</a>
+      <li><a href="https://x.com/brianjgamble" target="_blank" rel="noreferrer noopener">X</a>
+      <li><a href="https://www.linkedin.com/in/bjgamble/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
 |]
