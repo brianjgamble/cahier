dd5d3dad100edfab84c5e86164a112671e963b09
Brian Gamble <brianjgamble@users.noreply.github.com>
2026-03-14 21:14:39 -0500

    Create robots.txt in static

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,6 @@ import Cahier.Web.Presenters.Poetry qualified as Poetry
 
 import Cahier.Web.Logger (logStdoutCustom)
 import Config (scottyOptions)
-import Data.Text.Lazy qualified as TL
 import Network.Wai.Middleware.Static
 import Text.Blaze.Html (Html)
 import Text.Blaze.Html.Renderer.Text qualified as R
@@ -23,10 +22,6 @@ main = do
   scottyOpts opts do
     middleware $ staticPolicy (noDots >-> addBase "static")
     middleware logger
-
-    get "/robots.txt" $ do
-      setHeader "Content-Type" "text/plain"
-      text robotsTxt
 
     get "/" $ render Pages.home
 
@@ -46,101 +41,3 @@ main = do
 
 render :: Html -> ActionM ()
 render = html . R.renderHtml
-
-robotsTxt :: TL.Text
-robotsTxt =
-  TL.pack
-    """
-    User-agent: *
-    Allow: /
-
-    # ---------------------------
-    # OpenAI
-    # ---------------------------
-    User-agent: GPTBot
-    Disallow: /
-
-    # ---------------------------
-    # Google AI training
-    # ---------------------------
-    User-agent: Google-Extended
-    Disallow: /
-
-    # ---------------------------
-    # Anthropic
-    # ---------------------------
-    User-agent: ClaudeBot
-    Disallow: /
-
-    # ---------------------------
-    # Perplexity
-    # ---------------------------
-    User-agent: PerplexityBot
-    Disallow: /
-
-    # ---------------------------
-    # Amazon AI
-    # ---------------------------
-    User-agent: Amazonbot
-    Disallow: /
-
-    # ---------------------------
-    # Apple AI training
-    # ---------------------------
-    User-agent: Applebot-Extended
-    Disallow: /
-
-    # ---------------------------
-    # ByteDance / TikTok
-    # ---------------------------
-    User-agent: Bytespider
-    Disallow: /
-
-    # ---------------------------
-    # Common Crawl (many AI datasets)
-    # ---------------------------
-    User-agent: CCBot
-    Disallow: /
-
-    # ---------------------------
-    # Meta / Facebook research crawlers
-    # ---------------------------
-    User-agent: FacebookBot
-    Disallow: /
-
-    User-agent: Meta-ExternalAgent
-    Disallow: /
-
-    # ---------------------------
-    # AI dataset & scraping companies
-    # ---------------------------
-    User-agent: Diffbot
-    Disallow: /
-
-    User-agent: DataForSeoBot
-    Disallow: /
-
-    User-agent: ImagesiftBot
-    Disallow: /
-
-    User-agent: OAI-SearchBot
-    Disallow: /
-
-    # ---------------------------
-    # Misc aggressive scrapers
-    # ---------------------------
-    User-agent: PetalBot
-    Disallow: /
-
-    User-agent: SemrushBot
-    Disallow: /
-
-    User-agent: AhrefsBot
-    Disallow: /
-
-    User-agent: MJ12bot
-    Disallow: /
-
-    User-agent: DotBot
-    Disallow: /
-    """
